f167f03ec4a9ad90d0be8ebec178297f58f2316e
punchdii <sunqiche@outlook.com>
2025-01-07 11:12:52 -0500

    added funcionality of grabbing ip address using the ip-api api. API is called from server and received in json format.

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { BlogPosts } from 'app/components/posts'
+import { VisitorLocation } from 'app/components/VisitorLocation'
 
 export default function Page() {
   return (
@@ -7,6 +8,7 @@ export default function Page() {
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
         My name is Lucas Sun!
       </h1>
+      <VisitorLocation />
       <p className="mb-4 ">
         {`I'm a computer engineering student at University of Waterloo. I do full stack development, firmware at school rocketry team, and working on building a interpreter/compiler in Go! Also checkout my photography work `}<a className="mb-4 text-black-500 underline" href='https://www.instagram.com/punchdii/'>here</a>
       </p>
