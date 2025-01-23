0ad7e9522771477f8af10eb0aa89264eae3b9a53
punchdii <sunqiche@outlook.com>
2025-01-22 21:49:53 -0500

    text modification

--- app/page.tsx
+++ app/page.tsx
@@ -11,12 +11,15 @@ export default function Page() {
       <VisitorLocation />
       <p className="text-sm text-gray-600">If you are an employer checkout my <a className='underline text-sm text-gray-600' href='/resume_2024_12_24_hardware.pdf'>resume</a> </p>
       <p className="mb-4 ">
-        {`I'm a computer engineering student at University of Waterloo. I do full stack development, firmware at school rocketry team, and I'm currently working on building a interpreter/compiler in Go! Also checkout my photography work `}<a className="mb-4 text-black-500 underline" href='https://www.instagram.com/punchdii/'>here</a>
+        {`I'm a computer engineering student at University of Waterloo. I build websites, make robots move and write code for chips that go up with rockets. I'm currently working on building an interpreter/compiler in GO! Also trying to learn a bit more about FPGA and learn Linux enough to admin a server! Also checkout my photography work `}<a className="mb-4 text-black-500 underline" href='https://www.instagram.com/punchdii/'>here</a>.
+      </p>
+      <p className="mb-4 ">
+        Learn more about me at my <a className="mb-4 text-black-500 underline" href='/portfolio'>portfolio</a>.
       </p>
 
       <div className="my-8">
         <BlogPosts />
       </div>
-    </section>
+    </section >
   )
 }
